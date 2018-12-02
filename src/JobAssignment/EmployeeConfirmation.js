6d4151358e5bc1414ef92e5fa56a2940687d248f
WilliamG240 <williamnono94@hotmail.com>
2018-12-01 19:18:29 -0500

    Revert "Merge pull request #40 from CSC207-TLI-TBTEENS/searchbarjobassign"
    This reverts commit 7f5d1a460fa3f3f85651d8d42db0e62b01ea4232, reversing
    changes made to ce192de8c6819a8a854820019d7cb2584747912d.

--- src/JobAssignment/EmployeeConfirmation.js
+++ src/JobAssignment/EmployeeConfirmation.js
@@ -1,67 +1,49 @@
-import React, { Component } from 'react';
-import JobsList from './JobsList.js';
-import EmployeesList from './EmployeesList.js';
-import TaskConfirmation from './TaskConfirmation.js';
-import Confirmation from './Confirmation.js';
-import * as jobAPI from '../ClientJobs/api.js';
-import * as employeeAPI from '../Employees/api.js';
-import './JobAssignment.css';
-import 'element-theme-default';
-import JobAssignment from "./JobAssignment";
+import React from 'react';
 
-export class EmployeeConfirmation extends Component {
+const tempJobs = ["Fix truck number 25", "Fix crane number 30", "Dig this mine"]
 
-  state = {
-    jobs: this.props.jobs,
-    currentEmployee: this.props.currentEmployee
-  };
-
-  render() {
-    return (
-      <div class="card card-scroll">
+const EmployeeConfirmation = (props) => { 
+  return (
+    <div class="card card-scroll">
         <div class="card-body">
-          <h4 class="card-title">{this.state.currentEmployee.firstname + ' ' + this.state.currentEmployee.lastname}</h4>
-          <h6 class="card-subtitle mb-2">Employee's Tasks Queue</h6>
-          {
-            this.state.jobs.map(job => {
-              return (
-                <div>
-                  <p className="card-text closer-p">
-                    <i className="el-icon-delete el-icon-left" data-toggle="modal"
-                       data-target="#deleteJobCenterModal"></i>
-                    {job}
-                  </p>
+            <h4 class="card-title">{props.currentEmployee.firstname + ' ' + props.currentEmployee.lastname}</h4>
+            <h6 class="card-subtitle mb-2">Employee's Tasks Queue</h6>
+            {
+                tempJobs.map(job => {
+                    return (
+                        <div>
+                            <p className="card-text closer-p">
+                                <i className="el-icon-delete el-icon-left" data-toggle="modal" data-target="#deleteJobCenterModal"></i>
+                                {job}
+                            </p>
 
-                  <div class="modal fade" id="deleteJobCenterModal" tabindex="-1" role="dialog"
-                       aria-labelledby="ModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal fade" id="deleteJobCenterModal" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
+                              <div class="modal-dialog modal-dialog-centered" role="document">
+                                
+                                <div class="modal-content modal-content-confirm">
+                                  
+                                  <div class="modal-header modal-header-confirm">
+                                    <h4 class="modal-title" id="ModalCenterTitle">This will delete this job from this employee. Continue?</h4>
+                                  </div>
 
-                      <div class="modal-content modal-content-confirm">
+                                  <div class="modal-footer modal-footer-confirm">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                    <button type="button" class="btn btn-confirmation" data-dismiss="modal">OK</button>
+                                  </div>
 
-                        <div class="modal-header modal-header-confirm">
-                          <h4 class="modal-title" id="ModalCenterTitle">This will delete this job from this employee.
-                            Continue?</h4>
+                                </div>
+
+                              </div>
+                            </div>
+                            
                         </div>
-
-                        <div class="modal-footer modal-footer-confirm">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                          <button type="button" class="btn btn-confirmation" data-dismiss="modal">OK</button>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-
-                </div>
-              )
-            })
-          }
+                    )
+                })
+            }
         </div>
-      </div>
-    )
-  }
-
+    </div>
+  )
 }
+
 
 export default EmployeeConfirmation;
--- src/JobAssignment/JobAssignment.js
+++ src/JobAssignment/JobAssignment.js
@@ -60,7 +60,6 @@ export class JobAssignment extends Component {
     }
 
     render() {
-        let jobs = this.getJobsFromEmployee(this.state.employeeToConfirm, this.state.tasks);
         return (
             <div className="container h-80">
                 <div class="row justify-content-center align-items-center">
@@ -91,7 +90,7 @@ export class JobAssignment extends Component {
                         </div>
                         <div className="row align-items-center justify-content-center">
                             <div className="col-md-12 mb-3 d-flex justify-content-center">
-                                <EmployeeConfirmation currentEmployee={this.state.employeeToConfirm} jobs={[]}/>
+                                <EmployeeConfirmation currentEmployee={this.state.employeeToConfirm}/>
                             </div>
                          </div>
                         <div className="row justify-content-center">
