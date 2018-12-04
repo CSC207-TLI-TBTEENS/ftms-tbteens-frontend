b53f7f1d8dfd543dc03cce647bc2c039a456122a
lukekwong <44675873+lukekwong@users.noreply.github.com>
2018-12-04 03:33:25 -0500

    Make job assign take real jobs (#51)
    * Modified JobAssignment scripts to fetch values from database (jobs for selected employee and employee for selected job)
    
    * JobAssignment is getting and returning data from the server in real-time

--- src/JobAssignment/JobAssignment.js
+++ src/JobAssignment/JobAssignment.js
@@ -9,12 +9,20 @@ import * as employeeAPI from '../Employees/api.js';
 import 'element-theme-default';
 
 export class JobAssignment extends Component {
-    state = {
+    constructor(props) {
+      super(props);
+      this.state = {
         jobs: [],
         employees: [],
         tasks: [],
-        jobToConfirm: {jobTitle: "No Task Chosen", employees: []},
-        employeeToConfirm: {firstname: "No Employee Chosen", lastname: ""}
+        jobToConfirm: {jobTitle: "No Job Chosen", employees: []},
+        employeeToConfirm: {firstname: "No Employee Chosen", lastname: ""},
+        employeeJobs: [],
+        jobEmployees: []
+      };
+
+      this.getJobsFromEmployees = this.getJobsFromEmployee.bind(this);
+      this.getEmployeesFromJob = this.getEmployeesFromJob.bind(this);
     }
 
     componentWillMount() {
@@ -32,10 +40,19 @@ export class JobAssignment extends Component {
         this.setState({employees: allEmployees});
     }
 
-    async getJobsFromEmployee(employee, tasks) {
-        let inputs = [employee, tasks];
-        let jobsFromEmloyee = await employeeAPI.getJobsFromEmployee({...inputs});
-        return jobsFromEmloyee;
+    async getJobsFromEmployee(employee) {
+        let jobsFromEmployee = await employeeAPI.getJobsFromEmployee(employee.id);
+        this.setState({employeeJobs: jobsFromEmployee});
+    }
+
+    async getEmployeesFromJob(job) {
+        let employeesFromJob = await jobAPI.getEmployeesFromJob(job.id);
+        this.setState({jobEmployees: employeesFromJob});
+    }
+
+    // Just a dummy until I have time to implement and test the real onclick for deleting workers/jobs from each other.
+    async dummyOnClick() {
+        alert("Button pressed");
     }
 
     handleTaskChosen = (job) => {
@@ -43,27 +60,27 @@ export class JobAssignment extends Component {
             {
                 jobToConfirm: {...job}
             }
-        )
-        console.log(this.state.jobToConfirm)
-
-    }
+        );
+        this.getEmployeesFromJob({...job});
+        console.log(JSON.stringify(this.state.jobEmployees));
+    };
 
     handleEmployeeChosen = (employee) => {
-        // let jobsFromEmployee = this.getJobsFromEmployee(employee, this.state.tasks);
         this.setState(
             {
                 employeeToConfirm: {...employee}
             }
-        )
-        console.log(this.state.employeeToConfirm)
-    }
+        );
+        this.getJobsFromEmployee({...employee});
+        console.log(JSON.stringify(this.state.employeeJobs));
+    };
 
     render() {
         return (
             <div className="container h-80">
                 <div class="row justify-content-center align-items-center">
                     <div class="col-4 align-items-center job-manage-header">
-                        <h3>Jobs Manager</h3>
+                        <h3>Assign Job</h3>
                     </div>
                 </div>
                 <div class="row justify-content-between align-items-center">
@@ -76,25 +93,25 @@ export class JobAssignment extends Component {
                         <h3>Jobs</h3>
                     </div>
                 </div>
+                {/*The main content*/}
                 <div class="row justify-content-center h-100">
                     <div className="col-md-3 d-flex justify-content-center">
-                            <EmployeesList employees={this.state.employees} 
-                                employeeHandler={this.handleEmployeeChosen}/> 
+                            <EmployeesList employees={this.state.employees} employeeHandler={this.handleEmployeeChosen}/>
                     </div>
                     <div className="col-md-6">
                         <div className="row align-items-center justify-content-center">
                             <div className="col-md-12 mb-3 d-flex justify-content-center">
-                                <TaskConfirmation currentJob={this.state.jobToConfirm}/>
+                                <TaskConfirmation currentJob={this.state.jobToConfirm} employees={this.state.jobEmployees}/>
                             </div>
                         </div>
                         <div className="row align-items-center justify-content-center">
                             <div className="col-md-12 mb-3 d-flex justify-content-center">
-                                <EmployeeConfirmation currentEmployee={this.state.employeeToConfirm}/>
+                                <EmployeeConfirmation currentEmployee={this.state.employeeToConfirm} jobs={this.state.employeeJobs} onClick={this.dummyOnClick()}/>
                             </div>
                          </div>
                         <div className="row justify-content-center">
                             <div className="col-md-12 d-flex justify-content-center">
-                                <Confirmation employee={this.state.employeeToConfirm} job={this.state.jobToConfirm} />
+                                <Confirmation employee={this.state.employeeToConfirm} job={this.state.jobToConfirm} onClick={this.dummyOnClick()}/>
                             </div>
                         </div>
                     </div> 
