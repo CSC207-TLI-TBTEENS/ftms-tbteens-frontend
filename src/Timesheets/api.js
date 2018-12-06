2792654804d0b80726dc81ba5399bdf0ed710702
Hao Yu <hpeiyu.yu@mail.utoronto.ca>
2018-12-05 19:32:36 -0500

    Edit Task Details

--- src/Timesheets/api.js
+++ src/Timesheets/api.js
@@ -24,16 +24,7 @@ export async function deleteTask(input) {
     return apiCall('DELETE', address, {...input});
 }
 
-// //Edit Task
-// export async function editTask(input) {
-//     let id = input.id.toString();
-//     let taskname = input.taskname;
-//     let starttime = input.starttime;
-//     let endtime = input.endtime;
-//     let address = (TASKAPI + id + "?taskname=" + taskname + "&starttime=" + starttime + "&endtime=" + endtime);
-//     return apiCall('PUT', address, {...input});
-    
-// }
+
 
 //Below is for one spcific Task
 
@@ -44,4 +35,19 @@ export async function editSession(input) {
     let endtime = input.endtime;
     let address = (SESSIONAPI + id + "&starttime=" + starttime + "&endtime=" + endtime);
     return apiCall('PUT', address, {...input});
+}
+
+//Edit Task
+export async function editTask(input) {
+    let id = input.id.toString();
+    let taskName = input.taskName;
+    let taskDescription = input.taskDescription
+    let startTime = input.overallStartTime;
+    let endTime = input.overallEndTime;
+    let duration = input.totalDuration;
+    
+    
+    let address = (SESSIONAPI + id + "?taskname=" + taskName + "&sdescription=" + taskDescription + "&st=" + startTime + "&et=" + endTime + "&duration=" + duration);
+    return apiCall('PUT', address, {...input});
+    
 }
